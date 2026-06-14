92b2586a0004ff9dc7b1112a8b3c66d25caadc83
Rafidul <103718278+Rafidul01@users.noreply.github.com>
2026-06-15 00:21:11 +0600

    Create Users table with specified fields

--- QUERY.sql
+++ QUERY.sql
@@ -1,1 +1,8 @@
-
+-- create users table
+create table Users (
+  user_id serial primary key,
+  full_name varchar(50) not null,
+  email varchar(50) unique not null,
+  role varchar(20) check (role in ('Ticket Manager', 'Football Fan')),
+  phone_number varchar(20)
+);
