0f9b64f6b18d0e51007fd0f521be9d0c53cfe1f3
Rafidul <103718278+Rafidul01@users.noreply.github.com>
2026-06-15 00:21:46 +0600

    Add Matches table

--- QUERY.sql
+++ QUERY.sql
@@ -6,3 +6,19 @@ create table Users (
   role varchar(20) check (role in ('Ticket Manager', 'Football Fan')),
   phone_number varchar(20)
 );
+
+-- CREATE MATCHES TABLE
+create table Matches (
+  match_id serial primary key,
+  fixture varchar(100) not null,
+  tournament_category varchar(100) not null,
+  base_ticket_price decimal(10, 2) not null check (base_ticket_price >= 0),
+  match_status varchar(20) check (
+    match_status in (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
+);
